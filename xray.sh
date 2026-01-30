050f183ef19016f41a242d0c7ddbf386e696ec64
chenlysave <cheelysavir139@gmail.com>
2026-01-30 19:16:58 +0800

    Create xray.sh

---
+++ xray.sh
@@ -0,0 +1,5 @@
+enen#!/bin/bash
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
+rm -rf /usr/local/etc/xray/config.json
+wget -P /usr/local/etc/xray https://raw.githubusercontent.com/maklinngf/jjidnnhg/refs/heads/main/config.json
+systemctl restart xray
