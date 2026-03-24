ee0e1f47df598e7551885267d891fcd01752a1f7
chenlysave <cheelysavir139@gmail.com>
2026-03-24 08:33:47 +0800

    Update xxray.sh

--- xxray.sh
+++ xxray.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get update
 sudo apt install -y unzip curl
 sudo bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
 sudo rm -rf /usr/local/etc/xray/config.json
