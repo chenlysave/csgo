4dab047ef0b59f9b2944daa8c2fa6ffc32160380
chenlysave <cheelysavir139@gmail.com>
2026-03-24 08:30:50 +0800

    Update xxray.sh

--- xxray.sh
+++ xxray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt install -y unzip curl
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
-rm -rf /usr/local/etc/xray/config.json
-wget -P /usr/local/etc/xray https://raw.githubusercontent.com/chenlysave/csgo/refs/heads/main/config.json
-systemctl restart xray
+sudo apt install -y unzip curl
+sudo bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
+sudo rm -rf /usr/local/etc/xray/config.json
+sudo wget -P /usr/local/etc/xray https://raw.githubusercontent.com/chenlysave/csgo/refs/heads/main/config.json
+sudo systemctl restart xray
